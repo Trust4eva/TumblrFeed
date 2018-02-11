0fc84a702dda304b39dba01b7b5f128d2d15ee5a
Trustin Harris <trust@Trustins-MacBook-Pro.local>
2018-02-11 17:39:49 -0500

    commit

--- TumblrFeed/PhotosViewController.swift
+++ TumblrFeed/PhotosViewController.swift
@@ -13,14 +13,14 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
-
+    
     var posts: [[String: Any]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
-
+        
         
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -32,7 +32,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
                 
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
-            
+                
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
                 self.tableView.reloadData()
                 
@@ -65,13 +65,13 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
         
         return cell
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
     
-
-  
-
+    
+    
+    
 }
